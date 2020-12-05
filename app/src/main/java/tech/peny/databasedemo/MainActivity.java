0d4dc7f750141340309ab5e357b270411aba0965
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-05 01:21:29 +0000

    [feat]:  Added databse
    Created database, created database table, insert data into table, pull data from table, print data to console

--- app/src/main/java/tech/peny/databasedemo/MainActivity.java
+++ app/src/main/java/tech/peny/databasedemo/MainActivity.java
@@ -2,7 +2,10 @@ package tech.peny.databasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users",MODE_PRIVATE,null);
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+        myDatabase.execSQL("INSERT INTO users (name,age) VALUES ('Prince',22)");
+        myDatabase.execSQL("INSERT INTO users (name,age) VALUES ('Hilda',23)");
+
+        Cursor c = myDatabase.rawQuery("SELECT * FROM users",null);
+        int nameIndex = c.getColumnIndex("name");
+        int ageIndex = c.getColumnIndex("age");
+
+        c.moveToFirst();
+        while(c != null){
+            Log.i("name", c.getString(nameIndex));
+            Log.i("age",Integer.toString(c.getInt(ageIndex)));
+            c.moveToNext();
+        }
     }
 }
